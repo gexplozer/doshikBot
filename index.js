a0f9e399ce409f339ba03560fde68c2bb19e1616
gexplozer <djemiloff@gmail.com>
2018-10-19 00:06:52 +0300

    начнём

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const bot = new Telegraf("679868232:AAErCx6ZBlQtwiolYL3VIo-TW1KPxQUkjDs");
 bot.on('text', (ctx) => {
 	ctx.reply(`Hello ${ctx.state.role}`)
 })
+console.log("hell");
+bot.startPolling();
