f4d36ae51b3656c34bec33210666b070faee8d79
gexplozer <djemiloff@gmail.com>
2018-10-18 23:54:04 +0300

    начнём

---
+++ index.js
@@ -0,0 +1,7 @@
+const Telegraf = require('telegraf');
+
+const bot = new Telegraf("679868232:AAErCx6ZBlQtwiolYL3VIo-TW1KPxQUkjDs");
+
+bot.on('text', (ctx) => {
+	ctx.reply(`Hello ${ctx.state.role}`)
+})
